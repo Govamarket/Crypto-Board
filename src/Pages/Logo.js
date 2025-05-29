26d4809dbb55f7e7a4783e937b7f907e9c055aaf
Govamarket <govaway@gmail.com>
2025-05-29 13:30:59 +0100

    initial commit

--- src/Pages/Logo.js
+++ src/Pages/Logo.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Logo = () => {
   return (
-    <div className="cursor-pointer font-semibold shadow ">
+    <div className="cursor-pointer font-semibold">
       <h1>Crypto Board</h1>
     </div>
   );
